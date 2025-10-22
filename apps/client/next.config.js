67808a3ea089042f5ed0710aeadd6f6f3876ce11
mspolermo <mspolermo@gmail.com>
2025-10-22 19:59:57 +0500

    Client: next config updated

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -3,6 +3,34 @@ const nextConfig = {
   sassOptions: {
     includePaths: ['./src/app/styles'],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'st.kp.yandex.net',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.mds.yandex.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kinopoiskapiunofficial.tech',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5001',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
