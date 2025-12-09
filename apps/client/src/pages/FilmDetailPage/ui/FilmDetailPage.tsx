a81efa66ffe5c414bbad290be5bd96f20308807f
mspolermo <mspolermo@gmail.com>
2025-12-09 16:40:27 +0500

    Client: FilmDetailPage refactored

--- apps/client/src/pages/FilmDetailPage/ui/FilmDetailPage.tsx
+++ apps/client/src/pages/FilmDetailPage/ui/FilmDetailPage.tsx
@@ -1,43 +1,18 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'next/navigation';
 import { Layout } from '@/widgets/Layout';
-import { TFilmWithProfessions } from '@common/types';
 import styles from './FilmDetailPage.module.scss';
-import { FilmDetail, FilmDetailSkeleton, getFilmById } from '@/entities/film';
+import { FilmDetail, FilmDetailSkeleton } from '@/entities/film';
 import { FilmCreatorsViewer } from '@/widgets/FilmCreatorsViewer';
+import { useFilmDetails } from '@/features/getFilmDetails';
 
-export const FilmDetailPage: React.FC = () => {
+export const FilmDetailPage = () => {
   const params = useParams();
   const filmId = Number(params?.id);
 
-  const [film, setFilm] = useState<TFilmWithProfessions | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchFilm = async () => {
-      try {
-        setLoading(true);
-        setError(null);
-        const filmData = await getFilmById(filmId);
-        setFilm(filmData);
-      } catch (err) {
-        console.error('Error fetching film:', err);
-        setError('Ошибка загрузки фильма');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (filmId) {
-      fetchFilm();
-    } else {
-      setError('ID фильма не найден');
-      setLoading(false);
-    }
-  }, [filmId]);
+  const { loading, error, film } = useFilmDetails(filmId)
 
   if (loading) {
     return (
